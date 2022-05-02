6f89f851ab36069827df0ecdf9c8e73a87c57ecc
Michael Dougherty <maackle.d@gmail.com>
2022-05-02 12:54:44 -0700

    Basic app suitable for basic test

---
+++ dna/src/lib.rs
@@ -0,0 +1,46 @@
+use hdk::prelude::*;
+
+#[hdk_entry(id = "item_entry")]
+#[derive(Clone)]
+pub struct Item(u32);
+
+entry_defs![Item::entry_def(), PathEntry::entry_def()];
+
+fn path_entry_hash() -> ExternResult<EntryHash> {
+    let path = Path::from("items");
+    path.ensure()?;
+    let path_eh = path.path_entry_hash()?;
+    Ok(path_eh)
+}
+
+#[hdk_extern]
+pub fn create(n: u32) -> ExternResult<()> {
+    let item = Item(n);
+    let _ = create_entry(item.clone())?;
+    let eh = hash_entry(item)?;
+
+    let path_eh = path_entry_hash()?;
+    create_link(path_eh, eh.clone(), LinkType(0), ())?;
+
+    Ok(())
+}
+
+#[hdk_extern]
+pub fn list(_: ()) -> ExternResult<List> {
+    let path_eh = path_entry_hash()?;
+    let links = get_links(path_eh, None)?;
+    let num_links = links.len();
+    let items: Vec<Item> = links
+        .into_iter()
+        .flat_map(|l| get(l.target, Default::default()).unwrap())
+        .flat_map(|el| el.entry.as_option().cloned())
+        .map(|e| Item::try_from(e).unwrap())
+        .collect();
+    Ok(List { num_links, items })
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct List {
+    num_links: usize,
+    items: Vec<Item>,
+}
---
+++ ui/main.js
@@ -0,0 +1,84 @@
+import { AdminWebsocket, AppWebsocket } from "@holochain/client"
+
+const adminPort = 3333
+const appPort = 4444
+const signalCb = () => { }
+
+
+switch (process.argv[2]) {
+    case 'i':
+        await install()
+        console.log('install complete')
+        break
+    case 'c':
+        await create()
+        console.log('create complete')
+        break
+    case 'l':
+        await list()
+        console.log('list complete')
+        break
+}
+
+async function install() {
+    const adminClient = await AdminWebsocket.connect(`ws://localhost:${adminPort}`, 12000, signalCb)
+    let agent_key = await adminClient.generateAgentPubKey()
+    await adminClient.uninstallApp({
+        installed_app_id: 'app',
+    })
+    console.log("installing app")
+    await adminClient.installAppBundle({
+        agent_key,
+        installed_app_id: 'app',
+        path: './dna/simple.happ',
+        membrane_proofs: {}
+    })
+    try {
+        console.log("attaching app interface")
+        await adminClient.attachAppInterface({
+            port: appPort
+        })
+    } catch (e) {
+        console.log("(already attached)")
+    }
+    adminClient.client.close()
+}
+
+async function create() {
+    const adminClient = await AdminWebsocket.connect(`ws://localhost:${adminPort}`, 12000, signalCb)
+    await adminClient.enableApp({ installed_app_id: 'app' })
+    const client = await AppWebsocket.connect(`ws://localhost:${appPort}`, 12000, signalCb)
+    const info = await client.appInfo({ installed_app_id: 'app' })
+    console.log(info)
+    const cell_id = info.cell_data[0].cell_id;
+    await client.callZome({
+        cap: null,
+        cell_id,
+        zome_name: "simple",
+        fn_name: 'create',
+        provenance: cell_id[1],
+        payload: Math.floor(Math.random() * (2 ** 31)),
+    }, 30000)
+    adminClient.client.close()
+    client.client.close()
+}
+
+async function list() {
+    const adminClient = await AdminWebsocket.connect(`ws://localhost:${adminPort}`, 12000, signalCb)
+    await adminClient.enableApp({ installed_app_id: 'app' })
+    const client = await AppWebsocket.connect(`ws://localhost:${appPort}`, 12000, signalCb)
+    const info = await client.appInfo({ installed_app_id: 'app' })
+    console.log(info)
+    const cell_id = info.cell_data[0].cell_id;
+    const items = await client.callZome({
+        cap: null,
+        cell_id,
+        zome_name: "simple",
+        fn_name: 'list',
+        provenance: cell_id[1],
+        payload: null,
+    }, 30000)
+    adminClient.client.close()
+    client.client.close()
+    console.log(JSON.stringify(items, null, 2))
+}
